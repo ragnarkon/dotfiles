a71a19b721d29ed83da46b2cddd607ef7e249f83
Bryan Woolsey <ragnarkon@gmail.com>
2026-04-08 19:46:04 -0400

    Add buildin plugins, don't fix ghostty

--- home/.config/nvim/lua/plugins/init.lua
+++ home/.config/nvim/lua/plugins/init.lua
@@ -1,3 +1,10 @@
+-- Turn on DiffTool
+vim.cmd('packadd! nvim.difftool')
+
+-- Enable experimental UI feature.
+require('vim._core.ui2').enable()
+
+
 require("plugins.ai")
 require("plugins.colorscheme")
 require("plugins.editor")
