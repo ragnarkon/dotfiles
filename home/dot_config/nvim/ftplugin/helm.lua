914476628d20324813e21476fd479822d4a666ef
Bryan Woolsey <ragnarkon@gmail.com>
2026-04-15 17:01:19 -0400

    Fix helm ftplugin

--- home/dot_config/nvim/ftplugin/helm.lua
+++ home/dot_config/nvim/ftplugin/helm.lua
@@ -1,2 +1,9 @@
-require('nvim-treesitter').install({ "helm" })
+-- Treesitter for syntax highlighting
+require("nvim-treesitter").install({ "helm", "yaml" })
+vim.treesitter.start()
+
+-- Language server
+require("mason-lspconfig").setup({ ensure_installed = { "helm_ls" } })
+vim.lsp.enable("helm_ls")
+
 require("helm-ls").setup()
