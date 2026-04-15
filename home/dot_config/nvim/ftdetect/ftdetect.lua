e3edfbbd3d25268c96890decad9a603877c137f7
Bryan Woolsey <ragnarkon@gmail.com>
2026-04-15 15:49:52 -0400

    Add helm ftdetect

---
+++ home/dot_config/nvim/ftdetect/ftdetect.lua
@@ -0,0 +1,14 @@
+vim.filetype.add({
+  pattern = {
+    [".*/templates/.*%.tpl"] = "helm",
+    [".*/templates/.*%.ya?ml"] = "helm",
+    [".*/templates/.*%.txt"] = "helm",
+    ["helmfile.*%.ya?ml"] = "helm",
+    ["helmfile.*%.ya?ml.gotmpl"] = "helm",
+    ["values.*%.yaml"] = "yaml.helm-values",
+  },
+  filename = {
+    -- TODO: enable if helm-ls supports Chart.yaml
+    -- ["Chart.yaml"] = "yaml.helm-chartfile",
+  },
+})
