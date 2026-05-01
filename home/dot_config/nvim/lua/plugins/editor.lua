58df41433af65a73a4ab2bb6dfff3eb7800102ff
Bryan Woolsey <ragnarkon@gmail.com>
2026-05-01 18:22:33 -0400

    Add buffers keymap for nvim

--- home/dot_config/nvim/lua/plugins/editor.lua
+++ home/dot_config/nvim/lua/plugins/editor.lua
@@ -89,6 +89,7 @@ end
 
 vim.keymap.set('n', '<leader><space>', require('telescope.builtin').find_files, { desc = "Telescope find files" })
 vim.keymap.set('n', '<leader>fg', vim.find_files_from_project_git_root, { desc = "Telescope find git files" })
+vim.keymap.set('n', '<leader>fb', require('telescope.builtin').buffers, { desc = "Telescope buffers" })
 vim.keymap.set('n', '<leader>rg', require('telescope.builtin').live_grep, { desc = "Telescope live grep" })
 vim.keymap.set('n', '<leader>xx', require('telescope.builtin').diagnostics, { desc = "Telescope diagnostics" })
 
